d50eb2b7d1819d8740ca95d53792de446c7d4b00
company.kukugg <464680326@qq.com>
2017-08-18 15:24:10 +0800

    company cp commit,use fastJosn

--- springBoot/src/main/java/com/main/boot/StartApp.java
+++ springBoot/src/main/java/com/main/boot/StartApp.java
@@ -2,15 +2,37 @@ package com.main.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 
 
 @SpringBootApplication
 public class StartApp {
 
+	
+	//注入bean 使用fastjon 解析json
+	@Bean
+	public HttpMessageConverters fastJsonHttpMessageConverters() {
+		FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+		FastJsonConfig fastJsonConfig = new FastJsonConfig();
+		fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+		fastConverter.setFastJsonConfig(fastJsonConfig);
+		HttpMessageConverter<?> converter = fastConverter;
+		return new HttpMessageConverters(converter);
+		
+	}
+	
+	
+	
+	
 	public static void main(String[] args) {
 
 		SpringApplication.run(StartApp.class, args);
-		System.out.println(4);
 	}
 	
 }
